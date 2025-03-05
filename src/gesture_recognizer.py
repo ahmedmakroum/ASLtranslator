5112e7cea73d0c41410c97756b737651bca8e380
ahmedmakroum <ahmedmakroum3@gmail.com>
2025-03-05 10:29:11 +0000

    fixes

--- src/gesture_recognizer.py
+++ src/gesture_recognizer.py
@@ -8,6 +8,10 @@ class HandTracker:
         self.hands = self.mp_hands.Hands()
         self.mp_draw = mp.solutions.drawing_utils
 
+    def recognize_gesture(self, hand_landmarks):
+        # Simulated response: Randomly choose between "OK" and "Hello"
+        return random.choice(["OK", "Hello"])
+
     def detect_hand(self, frame):
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = self.hands.process(rgb_frame)
@@ -16,8 +20,7 @@ class HandTracker:
             for hand_landmarks in results.multi_hand_landmarks:
                 self.mp_draw.draw_landmarks(frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
                 
-                # Simulated response: Randomly choose between "OK" and "Hello"
-                text = random.choice(["OK", "Hello"])
+                text = self.recognize_gesture(hand_landmarks)
                 cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         
         return frame
